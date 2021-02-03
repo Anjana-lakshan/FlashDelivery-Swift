fc039eab4e351b9b4ae90b1947e2025e4d81ca97
Anjana lakshan <55266848+Anjana-lakshan@users.noreply.github.com>
2021-02-03 18:50:48 +0530

    Initial Commit

---
+++ FlashDelivery-ios/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  FlashDelivery-ios
+//
+//  Created by Anjana Lakshan on 2/3/21.
+//  Copyright © 2021 Anjana Lakshan. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
